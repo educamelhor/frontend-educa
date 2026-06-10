c55322d59b569ec87707353c8c8036218434ee82
André Santos <educa.melhor.2025@gmail.com>
2026-06-09 22:49:24 -0300

    feat(professor): adiciona submenu Conselho de Classe no menu Professores - rota /professores/conselho + item no sidebar com CheckCircleIcon

--- src/components/layout/Sidebar.jsx
+++ src/components/layout/Sidebar.jsx
@@ -377,6 +377,14 @@ export default function Sidebar({ isOpen, onClose }) {
                   </Link>
                 </li>
               )}
+              <li>
+                <Link
+                  to="/professores/conselho"
+                  className={getSubmenuLinkClasses('/professores/conselho')}
+                >
+                  <CheckCircleIcon className="h-5 w-5 mr-2" /> Conselho de Classe
+                </Link>
+              </li>
             </ul>
 
             </>

7215dfb71b30785cd095457c35ddf2f8a1b9b156
André Santos <educa.melhor.2025@gmail.com>
2026-08-09 21:17:46 -0300

    fix(aph): corrige caminho de importacao da api

--- src/features/disciplinar/aph/AphSelecaoAluno.jsx
+++ src/features/disciplinar/aph/AphSelecaoAluno.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useMemo } from 'react';
-import api from '../../../../services/api';
+import api from '../../../services/api';
 import { 
   ChevronRightIcon, 
   UserCircleIcon, 
